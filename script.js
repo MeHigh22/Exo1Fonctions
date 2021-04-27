5bc9e39a155d72aaadd8b8a20072cc20b6f3ebfb
MeHigh22 <pmihai31@gmail.com>
2021-04-27 13:37:09 +0200

    Exo1

---
+++ script.js
@@ -0,0 +1,66 @@
+// function addition(num1,num2) {
+//     return num1 + num2
+// }
+
+// console.log(addition(20, 70));
+
+// function soustraction(num1, num2) {
+//     return num1 - num2
+// }
+// console.log(soustraction(5,50));
+
+
+
+// function multiplication (num1, num2) {
+//      return num1 * num2
+// }
+
+// console.log(multiplication(5, 50));
+
+// function division(num1, num2) {
+//     return num1 / num2
+// }
+
+// console.log(division(5, 10));
+
+// function modulo(num1, num2){
+//     return num1 % num2
+// }
+
+// console.log(modulo(5,2));
+
+// function carre(num){
+//     return num * num
+// }
+
+// console.log(carre(5));
+
+// function expo(num){
+//     return num*num*num
+// }
+
+// console.log(expo(3));
+
+// function capitalize(mot){
+//     return mot.charAt(0).toUpperCase() + mot.slice(1);
+// }
+
+// console.log(capitalize("sahbe"));
+
+function calcul(nbr1, operator, nbr2){
+    switch(operator){
+        case "+":
+            return nbr1 + nbr2;
+        case '-':
+            return nbr1 - nbr2;
+        case '*':
+            return nbr1*nbr2;
+        case "/":
+            return nbr1 / nbr2
+        default:
+            return "Ce n'est pas un opérateur"
+    }
+}
+
+
+console.log(calcul(70,"+",20));
